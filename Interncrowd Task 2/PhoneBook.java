d161ac3002ab656301de96da92a756abcb27a175
Adarsh391 <110277795+Adarsh391@users.noreply.github.com>
2023-04-05 21:23:38 +0530

    Add files via upload

---
+++ Interncrowd Task 2/PhoneBook.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class PhoneBook {
+
+    private ArrayList<Contact> contacts;
+
+    public PhoneBook() {
+        contacts = new ArrayList<Contact>(); // arraylist for storing contacts
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public Contact findContact(String name) {
+        for (Contact contact : contacts) {
+            if (contact.getName().equals(name)) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    public void printContacts() {
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        PhoneBook phoneBook = new PhoneBook();
+        while (true) {
+            System.out.println("1. Add contact");
+            System.out.println("2. Find contact");
+            System.out.println("3. Print contacts");
+            System.out.println("4. Exit");
+            System.out.print("Enter choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            // switch case
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter phone number: ");
+                    String phoneNumber = scanner.nextLine();
+                    Contact contact = new Contact(name, phoneNumber);
+                    phoneBook.addContact(contact);
+                    break;
+                case 2:
+                    System.out.print("Enter name: ");
+                    name = scanner.nextLine();
+                    contact = phoneBook.findContact(name);
+                    if (contact == null) {
+                        System.out.println("Contact not found.");
+                    } else {
+                        System.out.println(contact);
+                    }
+                    break;
+                case 3:
+                    phoneBook.printContacts();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice.");
+            }
+            System.out.println();
+        }
+    }
+}
